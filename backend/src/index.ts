323a51fe906976e73ac16bfc6e1cda95a8ee59d8
Hemanth <hemanthks2000@gmail.com>
2024-03-14 01:22:43 +0530

    Initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import express, { Request,Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.get("/test", async (req: Request, res: Response)=>{
+    res.json({message: "Hello!"});
+});
+app.listen(7000, ()=>{
+    console.log("server started on localhost:7000") 
+});
