7ca58e226f49a8729ff760f6a616b9e9fab985cd
Hemanth <hemanthks2000@gmail.com>
2024-03-19 12:39:11 +0530

    Login with authentication created using auth0

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,8 +1,13 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 
 const MainNav = () => {
+  const {loginWithRedirect}=useAuth0();
   return(
-    <Button variant="ghost" className="font-bold text-white bg-orange-500 hover:bg-orange-700 hover:text-gray-300">Log In</Button>
+    <Button 
+    variant="ghost" 
+    className="font-bold text-white bg-orange-500 hover:bg-orange-700 hover:text-gray-300"
+    onClick={async () => await loginWithRedirect()}>Log In</Button>
   )
 }
 
